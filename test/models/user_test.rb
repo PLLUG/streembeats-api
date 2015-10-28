b930875fbe42a0890725212d5366a51012b946c1
Ivan Lavriv <lavriv92@gmail.com>
2015-10-29 00:23:57 +0200

    Add user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,6 +2,33 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
+  test "should uniqueness email and username" do
+    user = User.new do |u|
+      u.username = 'username'
+      u.email = 'email@example.com'
+    end
+    user.save
+
+    user_duplicated = User.new do |u|
+      u.username = 'username'
+      u.email = 'email@example.com'
+    end
+    user_duplicated.save
+    messages = user_duplicated.errors.messages
+
+    assert_not_empty messages[:email]
+    assert_not_empty messages[:username]
+  end
+
+  test "should username and email not empty" do
+    user = User.new
+    user.save
+    messages = user.errors.messages
+
+    assert_not_empty messages[:username]
+    assert_not_empty messages[:email]
+  end
+
   test "should create user" do
     user = User.new do |u|
       u.username = 'test_username'
