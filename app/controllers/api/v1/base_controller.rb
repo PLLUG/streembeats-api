e5a6a62ba584b77ec89e6c3ddaa9e37b7f48622f
Ivan Lavriv <lavriv92@gmail.com>
2015-11-04 18:09:56 +0200

    Added base controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -6,16 +6,15 @@ class Api::V1::BaseController < ApplicationController
 
   def before_crodomains
     header['Access-Control-Allow-Origin'] = "*"
-    header['Access-Control-Allow-Methods'] = 'POST,GET,PUT,DELETE,OPTIONS'
+    header['Access-Control-Allow-Methods'] = "POST,GET,PUT,DELETE,OPTIONS"
+    header['Access-Control-Allow-Headers'] = "Content-Type,Authorization"
   end
 
   def after_crossdomains
     header['Access-Control-Allow-Origin'] = "*"
-    header['Access-Control-Allow-Origin'] = ""
   end
 
   def authorize
     @current_user = UsersRepository.find_by_token(headers['Authorization'])
-    p @current_user
   end
 end
