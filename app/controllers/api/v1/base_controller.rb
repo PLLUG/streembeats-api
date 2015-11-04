6717915461a1f746c58cd9528907ed4d97a458ad
Ivan Lavriv <lavriv92@gmail.com>
2015-11-04 02:03:14 +0200

    Added assess control

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,6 +2,17 @@ class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session
 
   before_action :authorize
+  before_filter :resolve_crodomains
+
+  def before_crodomains
+    header['Access-Control-Allow-Origin'] = "*"
+    header['Access-Control-Allow-Methods'] = 'POST,GET,PUT,DELETE,OPTIONS'
+  end
+
+  def after_crossdomains
+    header['Access-Control-Allow-Origin'] = "*"
+    header['Access-Control-Allow-Origin'] = ""
+  end
 
   def authorize
     @current_user = UsersRepository.find_by_token(headers['Authorization'])
